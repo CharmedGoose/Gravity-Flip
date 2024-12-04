62d55a5a19095ab3c5e198031388ec13fd72f1ea
Goose <CharmedGoose1@gmail.com>
2024-12-04 14:45:11 +0000

    remove eslint

--- eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import sapphireEslintConfig from '@sapphire/eslint-config';
-
-const compat = new FlatCompat();
-
-export default [
-	...compat.config(sapphireEslintConfig)
-];
